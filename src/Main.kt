29bdcb16eafb2c1926e2dab3e7118bc958263e10
Bernhard Danecker <bernhard.danecker@gmail.com>
2017-10-20 15:13:40 +0200

    level2

--- src/Main.kt
+++ src/Main.kt
@@ -2,55 +2,107 @@ import java.io.File
 import java.util.concurrent.LinkedBlockingQueue
 
 //
-data class Account(val name: String, var balance: Long)
+data class Account(val name: String, val accountNumber: String, var balance: Long, val overdraftLimit: Long)
 
 data class Transaction(val from: String, val to: String, val amount: Long, val submitTime: Long)
 
 fun main(args: Array<String>) {
 
-    val level = "level1"
+    val level = "level2"
 
     File("input").listFiles({ dir, filename -> filename.startsWith(level) }).forEach {
-        processFile(it)
+        val result = processFile(it)
+        val joined = result.joinToString("\n")
+
+        println("--- OUTPUT ---")
+        println(joined)
+
+        File("output/" + it.name).apply {
+            delete()
+            createNewFile()
+            writeText(joined)
+        }
+
+        println()
     }
 }
 
-fun processFile(file: File) {
+fun processFile(file: File): Array<String> {
+    println("processing ${file.name}...")
+
     val lines = LinkedBlockingQueue(file.readLines())
 
     val numberOfAccounts = lines.poll().toInt()
     val accounts = (1..numberOfAccounts).map {
         val parts = lines.poll().split(" ")
-        Account(parts[0], parts[1].toLong())
+        Account(parts[0], parts[1], parts[2].toLong(), parts[3].toLong())
+    }.filter {
+        validateAccountNumber(it.accountNumber)
     }
 
-    val numerOfTransactions = lines.poll().toInt()
-    val transactions = (1..numerOfTransactions).map {
+    val numberOfTransactions = lines.poll().toInt()
+    val transactions = (1..numberOfTransactions).map {
         val parts = lines.poll().split(" ")
         Transaction(parts[0], parts[1], parts[2].toLong(), parts[3].toLong())
     }
 
-    transactions.forEach {
+    transactions.sortedBy { it.submitTime }.forEach {
         processTransaction(it, accounts)
     }
 
+    val result = arrayOf(accounts.count().toString()) + accounts.map { "${it.name} ${it.balance}" }
 
-    val result = accounts.map { "${it.name} ${it.balance}" }.joinToString("\n")
+    return result
+}
 
-    File("output/" + file.name).apply {
-        delete()
-        createNewFile()
-        appendText(accounts.count().toString())
-        appendText("\n")
-        appendText(result)
+fun validateAccountNumber(accountNumber: String): Boolean {
+    if (!accountNumber.startsWith("CAT")) {
+        println("invalid accountNumber, not starting with CAT: " + accountNumber)
+        return false
     }
+
+    val checksum = accountNumber.substring(3, 5).toInt()
+    val accountId = accountNumber.substring(5)
+
+    if (accountId.matches(Regex("[a-zA-Z]{10}")).not()) {
+        println("invalid accountNumber, not 10 characters: " + accountNumber)
+        return false
+    }
+
+    val asciiCodes = accountId.toList().map { it.toInt() }
+    val characterCounts = asciiCodes.groupBy { it }.mapValues { it.value.count() }
+    val equalLowercaseUppercase = ('A'.toInt()..'Z'.toInt()).all {
+        val uppercaseCount = characterCounts.getOrElse(it, { 0 })
+        val lowercaseCount = characterCounts.getOrElse(it + 32, { 0 })
+
+        lowercaseCount == uppercaseCount
+    }
+    if (!equalLowercaseUppercase) {
+        println("invalid accountNumber, no equal count: " + accountNumber)
+        return false
+    }
+
+    val calculatedChecksum = 98 - "${accountId}CAT00".sumBy { it.toInt() }.rem(97)
+    if (calculatedChecksum != checksum) {
+        println("invalid accountNumber, invalid calculated checksum: $calculatedChecksum, checksum: $checksum")
+        return false
+    }
+
+    return true
 }
 
 fun processTransaction(transaction: Transaction, accounts: List<Account>) {
 
-    val from = accounts.single { it.name == transaction.from }
-    val to = accounts.single { it.name == transaction.to }
+    val from = accounts.singleOrNull { it.accountNumber == transaction.from }
+    val to = accounts.singleOrNull { it.accountNumber == transaction.to }
 
-    from.balance -= transaction.amount
-    to.balance += transaction.amount
+    if (from != null && to != null) {
+        val targetAmout = from.balance - transaction.amount
+        if (targetAmout > -from.overdraftLimit) {
+            from.balance -= transaction.amount
+            to.balance += transaction.amount
+        } else {
+            println("target amount of $targetAmout over overdraftLimit ${from.overdraftLimit}")
+        }
+    }
 }
